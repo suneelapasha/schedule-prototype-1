56536769a4c339e3c3a66a7a3d9f8c10982e9db1
suneela <suneela.pasha@gmail.com>
2023-06-15 17:54:28 +0500

    minor slash

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/schedule-prototype-1",
+  base: "/schedule-prototype-1/",
   plugins: [react()],
 })
